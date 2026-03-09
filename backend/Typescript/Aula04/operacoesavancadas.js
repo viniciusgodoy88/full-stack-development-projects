d9dbc5a6a69829298e7c7911ed1773033c6919b1
Vinicius Ermiliato de Godoy <viniciusgodoy88@gmail.com>
2026-03-09 19:40:54 -0300

    Create operacoesavancadas.js

---
+++ backend/Typescript/Aula04/operacoesavancadas.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MatematicaAvancada = void 0;
+// Exporta o namespace
+var MatematicaAvancada;
+(function (MatematicaAvancada) {
+    function exponenciar(base, expoente) {
+        return Math.pow(base, expoente);
+    }
+    MatematicaAvancada.exponenciar = exponenciar;
+})(MatematicaAvancada || (exports.MatematicaAvancada = MatematicaAvancada = {}));
