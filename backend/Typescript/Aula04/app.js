fe3cacc09d8663758e6a85449abf4f27143202ac
Vinicius Ermiliato de Godoy <viniciusgodoy88@gmail.com>
2026-03-09 19:40:48 -0300

    Create app.js

---
+++ backend/Typescript/Aula04/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var operacoesbasicas_js_1 = require("./operacoesbasicas.js");
+var operacoesavancadas_js_1 = require("./operacoesavancadas.js");
+console.log(operacoesbasicas_js_1.MatematicaBasica.somar(5, 3)); // 8
+console.log(operacoesbasicas_js_1.MatematicaBasica.subtrair(5, 3)); // 2
+console.log(operacoesavancadas_js_1.MatematicaAvancada.exponenciar(2, 3)); // 8
