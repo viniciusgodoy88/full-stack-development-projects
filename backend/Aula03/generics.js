7932107fd2ef1a210f00ce2dfb6ec01a6221862f
Vinicius Ermiliato de Godoy <viniciusgodoy88@gmail.com>
2026-03-09 13:44:15 -0300

    Delete generics.js

--- backend/Aula03/generics.js
+++
@@ -1,9 +0,0 @@
-//Função em generics onde trata-se de um array que pode-se utilizar diversas tipos de dados, como string, inteiros,
-//dentre outros
-function primeiroElemento(arr) {
-    return arr[0];
-}
-var numeros = primeiroElemento([1, 2, 3]);
-var palavras = primeiroElemento(['Olá', 'mundo']);
-console.log(numeros);
-console.log(palavras);
